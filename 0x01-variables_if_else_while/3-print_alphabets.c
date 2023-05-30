4f6a34fb9360ce47cc495cfa61a854da61387d1f
elshaday zewdie <elshadayzb21@gmail.com>
2023-05-30 11:27:15 +0300

    compiled if else file

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,19 +5,19 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void){
-
-char c ;
-
-for( c = 'a'; c <= 'z'; c++ )
+int main (void)
 {
-   putchar(c);
-}
-for( c = 'A'; c <= 'Z'; c++ )
-{
-   putchar(c);
-   putchar('\n');
-}
+    char c;
 
-  return(0);
+    for(c = 'a'; c <= 'z'; c++ )
+    {
+        putchar(c);
+    }
+    for( c = 'A'; c <= 'Z'; c++ )
+    {
+        putchar(c);
+    }
+    putchar('\n');
+
+    return (0);
 }

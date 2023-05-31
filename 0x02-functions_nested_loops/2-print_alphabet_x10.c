e2cee2f0aab59b2fd283ccd3745f086c90990a40
elshaday zewdie <elshadayzb21@gmail.com>
2023-05-31 11:41:31 +0300

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,7 @@
 #include "main.h"
-
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * print_alphabet_x10 - printing alphabets 10 times
  */
-
 void print_alphabet_x10(void)
 {
     int count = 0;
@@ -15,9 +11,9 @@ void print_alphabet_x10(void)
     {
         for (c = 'a'; c <= 'z'; c++)
         {
-        putchar(c);
+            _putchar(c);
         }
-    putchar('\n');
+        _putchar('\n');
         count++;
     }
 }

6b91111729253ce03bb78d0f4b4724e57f45ee90
elshaday zewdie <elshadayzb21@gmail.com>
2023-05-31 11:56:21 +0300

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_sign - check a number is positive or negative or zero
+ * @n: the number to be checked
+ *
+ * Return: 1 if +ve or 0 if 0 or -1 if -ve
+ */
+int print_sign(int n)
+{
+    
+}

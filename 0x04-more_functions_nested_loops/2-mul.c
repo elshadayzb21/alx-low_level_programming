e8a0b4e1e561fe08ccb4b3333e58d3720620aa47
elshaday zewdie <elshadayzb21@gmail.com>
2023-06-01 14:52:32 +0300

    0x03-debugging

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 
-// mul - to check if the multiplication is carried on
-// @a : the first parameter
-// @b : the second parameter
-// return - return depending on the value
+/**
+* mul - to check if the multiplication is carried on
+* @a: the first parameter
+* @b: the second parameter
+* return - return depending on the value
+*/
 
 int mul(int a, int b)
 {

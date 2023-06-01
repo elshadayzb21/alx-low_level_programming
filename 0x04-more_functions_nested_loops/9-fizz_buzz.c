860938e4597c6a1610e9a08e1730ef64c6db8c2b
elshaday zewdie <elshadayzb21@gmail.com>
2023-06-01 16:29:16 +0300

    0x03-debugging

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Fizz for multiples of 3 and Buzz for multiples of 5 FizzBuzz for both
+ *
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    
+}

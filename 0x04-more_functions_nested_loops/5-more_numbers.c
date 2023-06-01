dd8b3a07ee2b9b892f32a36a328f55e358965751
elshaday zewdie <elshadayzb21@gmail.com>
2023-06-01 15:09:46 +0300

    0x03-debugging

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,5 +8,21 @@
  */
 void more_numbers(void)
 {
-    
+    int i;
+    int x;
+
+    for (x = 0; x < 10; x++)
+    {
+        i = 0;
+        while (i <= 14)
+        {
+            if (i >= 10)
+            {
+                _putchar(i / 10 + '0');
+            }
+            _putchar(i % 10 + '0');
+            i++;
+        }
+        _putchar('\n');
+    }
 }

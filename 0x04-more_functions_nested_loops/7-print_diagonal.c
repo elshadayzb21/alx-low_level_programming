41da5494749b62b3af650317f92a642590c4b2a4
elshaday zewdie <elshadayzb21@gmail.com>
2023-06-02 11:29:00 +0300

    0x03-debugging

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,4 +8,21 @@
  */
 void print_square(int n)
 {
+    int i;
+    int spaces;
+
+    for (i = 0; i < n; i++)
+    {
+        for (spaces = 0; spaces < i; spaces++)
+        {
+            _putchar(' ');
+        }
+        _putchar('\\');
+        _putchar('\n');
+    }
+
+    if (n <= 0)
+    {
+        _putchar('\n');
+    }
 }

bcbf1370e6a9efd55c30bdb34f53437c134f9411
elshaday zewdie <elshadayzb21@gmail.com>
2023-06-02 16:01:03 +0300

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,17 +13,17 @@ int main(void)
 
     for (x = 1; x < 101; x++)
     {
-        if (x % 3 == 0)
+        if (x % 3 == 0 && x % 5 == 0)
         {
-            printf("%s", "Fizz ");
+            printf("%s", "FizzBuzz ");
         }
         else if (x % 5 == 0)
         {
             printf("%s", "Buzz " );
         }
-        else if (x % 3 == 0 && x % 5 == 0)
+        else if (x % 3 == 0)
         {
-            printf("%s", "FizzBuzz ");
+            printf("%s", "Fizz ");
         }
         else
         {

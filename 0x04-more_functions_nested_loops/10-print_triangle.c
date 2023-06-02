c7aea7fdb1ebb654a9c002d38b0c23ce1d512c89
elshaday zewdie <elshadayzb21@gmail.com>
2023-06-02 16:11:18 +0300

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,4 +8,24 @@
  */
 void print_triangle(int size)
 {
+    int i;
+    int x;
+    int spaces;
+    ;
+    for (i = 0; i < size; i++)
+    {
+        for (spaces = size - 1 - i; spaces > 0; spaces--)
+        {
+            _putchar(' ');
+        }
+        for (x = 0; x <= i; x++)
+        {
+            _putchar('#');
+        }
+        _putchar('\n');
+    }
+    if (size <= 0)
+    {
+        _putchar('\n');
+    }
 }

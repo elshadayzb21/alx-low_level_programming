a10c4e7c975224d6eb95c3cec03c9b9357e14a2f
elshaday zewdie <elshadayzb21@gmail.com>
2023-06-05 08:57:36 +0300

    functions and nested loops more task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,26 +9,34 @@
  */
 int main(void)
 {
-    int x;
+	int x = 1;
 
-    for (x = 1; x < 101; x++)
-    {
-        if (x % 3 == 0 && x % 5 == 0)
-        {
-            printf("%s", "FizzBuzz ");
-        }
-        else if (x % 5 == 0)
-        {
-            printf("%s", "Buzz " );
-        }
-        else if (x % 3 == 0)
-        {
-            printf("%s", "Fizz ");
-        }
-        else
-        {
-            printf("%d ", x);
-        }
-    }
-    return (0);
+	while (x < 101)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("%s", "FizzBuzz");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("%s", "Fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("%s", "Buzz");
+		}
+		else
+		{
+			printf("%d", x);
+		}
+
+		if (x != 100)
+		{
+			printf(" ");
+		}
+		x++;
+	}
+	printf("\n");
+
+	return (0);
 }

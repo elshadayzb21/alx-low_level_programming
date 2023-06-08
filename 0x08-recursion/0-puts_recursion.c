69d0288817e5726610bb2c07ad965d3f58758c5b
elshaday zewdie <elshadayzb21@gmail.com>
2023-06-09 02:59:11 +0300

    0x08-recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,7 +8,8 @@ void _puts_recursion(char *s)
 {
     while (*s)
     {
-        _putchar( *s + _puts_recursion( s +1 ))
+        _putchar( *s );
+         _puts_recursion( s +1 );
     }
     _putchar('\n')
     
